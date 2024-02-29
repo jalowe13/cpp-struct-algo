7a726b1af8e46a10daf1851d4b0f36869417fc33
jalowe13 <jplowe21@vt.edu>
2024-02-29 13:22:19 -0600

    action start stop

--- cpp/concepts/task_scheduler.cpp
+++ cpp/concepts/task_scheduler.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <stack>
 #include <string>
 #include <vector>
 
@@ -23,22 +25,57 @@ TaskState stringToTaskState(const std::string &state) {
 }
 
 struct Task {
+  int task_id;
   TaskState state;
   int start_time;
   int end_time;
-  Task(int start_time, int end_time)
-      : start_time(start_time), end_time(end_time) {
-    state = TaskState::READY;
+  Task(int task_id, TaskState state, int time)
+      : task_id(task_id), state(state) {
+    switch (state) {
+    case TaskState::RUNNING: {
+      start_time = time;
+      break;
+    }
+    case TaskState::COMPLETED: {
+      end_time = time;
+      break;
+    }
+    default:
+      break;
+    }
   }
+  int get_start_time() { return start_time; }
+  int get_end_time() { return end_time; }
 };
 
 // Read the string vector into multiple vectors for start time, end time, and
 // function action
-void parse_task_log(std::vector<std::string> &tasks) {
+std::stack<Task> parse_task_log(std::vector<std::string> &tasks) {
+  std::stack<Task> active_tasks;
   // Iterate over tasks
   for (size_t i = 0; i < tasks.size(); i++) {
+    std::string task_id, action, time;
+    char sep = ':';
+    std::istringstream iss(tasks[i]);
+    std::getline(iss, task_id, sep);
+    std::getline(iss, action, sep);
+    std::getline(iss, time, sep);
+
+    if (action == "start") {
+      Task new_task(std::stoi(task_id), TaskState::RUNNING, std::stoi(time));
+      active_tasks.push(new_task);
+    }
+    if (action == "stop") {
+      // pop from the FIFO stack
+      std::cout << "Ending task " << active_tasks.top().task_id
+                << " which started at " << active_tasks.top().get_start_time()
+                << std::endl;
+      active_tasks.pop();
+    }
+
     std::cout << tasks[i] << std::endl;
   }
+  return active_tasks;
 }
 
 // Read the task log into a string vector
